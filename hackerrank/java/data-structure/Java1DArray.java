143d4a91a0cd13b3f4e99354764d99d1e5e66cc1
Mincong HUANG <mincong.h@gmail.com>
2016-10-07 14:46:50 +0200

    add Java 1D Array

---
+++ hackerrank/java/data-structure/Java1DArray.java
@@ -0,0 +1,9 @@
+        // https://www.hackerrank.com/challenges/java-1d-array-introduction
+        // Declare array a here
+        if (n < 0) return;
+        int[] a = new int[n];
+        for(int i = 0 ; i < n; i++){
+            int val = scan.nextInt();
+            // Fill array a here
+            a[i] = val;
+        }
