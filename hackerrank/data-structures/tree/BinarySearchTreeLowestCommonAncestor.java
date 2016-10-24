2eaf6d97ea817a0e090878e6dbbdf5bc9adb7197
Mincong HUANG <mincong.h@gmail.com>
2016-10-24 21:20:04 +0200

    add BST - LCA

---
+++ hackerrank/data-structures/tree/BinarySearchTreeLowestCommonAncestor.java
@@ -0,0 +1,23 @@
+/**
+ * Written in Google Doc.
+ *
+ * Node is defined as :
+ * class Node {
+ *   int data;
+ *   Node left;
+ *   Node right;
+ * }  
+ */
+static Node lca(Node root, int v1, int v2) {
+  if (root == null || v1 == root.data || v2 == root.data) {
+    return root;
+  }
+  if (v1 < root.data && v2 < root.data) {
+    return lca(root.left, v1, v2);
+  }
+  if (v1 > root.data && v2 > root.data) {
+    return lca(root.right, v1, v2);
+  }
+  return root;
+}
+
