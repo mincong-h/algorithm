11ee991d85d2dd778f64afab6af7755b11f892b8
Mincong HUANG <mincong.h@gmail.com>
2019-03-02 21:51:33 +0100

    L283: Move Zeroes (v2)

--- leetcode/easy/MoveZeroes.java
+++ leetcode/easy/MoveZeroes.java
@@ -3,15 +3,13 @@ class Solution {
         int fast = 0;
         int slow = 0;
         while (fast < nums.length) {
-            while (fast < nums.length && nums[fast] == 0) {
+            if (nums[fast] == 0) {
                 fast++;
+            } else {
+                nums[slow] = nums[fast];
+                fast++;
+                slow++;
             }
-            if (fast == nums.length) {
-                break;
-            }
-            nums[slow] = nums[fast];
-            slow++;
-            fast++;
         }
         for (; slow < nums.length; slow++) {
             nums[slow] = 0;
