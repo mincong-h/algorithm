1550e0b3eb3d03359580c8c7e31ac8968d4c2f0b
Mincong HUANG <mincong.h@gmail.com>
2019-03-14 07:27:14 +0100

    L56: Merge Intervals

--- leetcode/medium/MergeIntervals.java
+++ leetcode/medium/MergeIntervals.java
@@ -7,36 +7,24 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
-public class Solution {
+class Solution {
     public List<Interval> merge(List<Interval> intervals) {
-        if (intervals == null || intervals.size() <= 1) {
-            return intervals;
-        }
+        // sort intervals
+        intervals.sort((i1, i2) -> i1.start - i2.start);
 
-        // Sort by ascending starting point using an anonymous Comparator
-        Collections.sort(intervals, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval i1, Interval i2) {
-                return Integer.compare(i1.start, i2.start);
+        // add to results, merge if needed
+        LinkedList<Interval> results = new LinkedList<>();
+        for (Interval i : intervals) {
+            if (!results.isEmpty()) {
+                Interval last = results.getLast();
+                // overlap
+                if (last.start <= i.start && i.start <= last.end) {
+                    last.end = Math.max(i.end, last.end);
+                    continue;
+                }
             }
-        });
-
-        List<Interval> merged = new LinkedList<>();
-        int start = intervals.get(0).start;
-        int end = intervals.get(0).end;
-
-        for (Interval interval : intervals) {
-            // if not overlap
-            if (end < interval.start) {
-                merged.add(new Interval(start, end));
-                // reset
-                start = interval.start;
-                end = interval.end;
-            } else {
-                end = Math.max(end, interval.end);
-            }
+            results.add(i);
         }
-        merged.add(new Interval(start, end));
-        return merged;
+        return results;
     }
 }
