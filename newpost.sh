5e321ab2b3725d2f96bddb3d92f1249182e4fb9f
Mincong HUANG <mincong.h@gmail.com>
2019-04-03 21:09:02 +0200

    Add template for markdown creation

---
+++ newpost.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+#    This script creates a new markdown post in /leetcode folder. The
+#    naming convention is: ${id}.${title}.md
+#
+#    Usage:
+#
+#        ./newpost.sh 1 https://leetcode.com/problems/two-sum/
+#
+if [[ $# -lt 2 ]]
+then
+    echo 'usage: newpost.sh 1 https://leetcode.com/problems/two-sum/'
+    exit 1
+fi
+
+id="$1"
+url="$2"
+name=$(echo "$url" | sed -E 's/.*problems\/(.*)\//\1/')
+
+projhome=$(cd "$(dirname "$0")" || exit; pwd)
+filename="${id}.${name}.md"
+filepath="${projhome}/leetcode/${filename}"
+
+if [[ -f "$filepath" ]]; then
+    echo "$filepath already exists."
+    exit 1
+fi
+
+cat << EOF >> "$filepath"
+# ${id} Title
+
+<${url}>
+
+## Idea
+
+How to solve the problem? Which strategy? Time estimation?
+Corner cases?
+
+## Solution
+
+```java
+// TODO
+```
+
+Runtime: ? ms (faster than ?%), Memory: ? MB (better than ?%)
+EOF
+
+echo "Post created: $filepath"
