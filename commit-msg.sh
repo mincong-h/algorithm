285b431fd60326909d00d4357d23d90a982ebc77
Mincong HUANG <mincong.h@gmail.com>
2019-04-13 21:29:32 +0200

    Add commit-msg hook

---
+++ commit-msg.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# An example hook script to check the commit log message.
+# Called by "git commit" with one argument, the name of the file
+# that has the commit message.  The hook should exit with non-zero
+# status after issuing an appropriate message if it wants to stop the
+# commit.  The hook is allowed to edit the commit message file.
+#
+# To enable this hook, rename this file to "commit-msg".
+grep -E '^L(\d+): [ \w]+ \(\d+ms\)$' $1 || {
+	echo >&2 "Incorrect commit message. Sample:\n\n    L123: LeetCode Problem Title (456ms)\n"
+	exit 1
+}
