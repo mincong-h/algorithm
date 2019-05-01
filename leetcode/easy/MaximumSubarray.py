930a7f737ef093ae61d34f53ed629e988f5e8757
Mincong HUANG <mincong.h@gmail.com>
2019-05-01 11:33:55 +0200

    L53: Maximum Subarray (0ms)

--- leetcode/easy/MaximumSubarray.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        m = nums[0];
-        s = nums[0];
-        for i in range(1, len(nums)):
-            s = max(s + nums[i], nums[i])
-            if m < s:
-                m = s
-        return m
