42bc4e15639d2aaefc972899128b9043d5ecf658
Mincong HUANG <mincong.h@gmail.com>
2019-05-05 17:39:42 +0200

    Hook: Allow prefixes "Hook" and "Docs"

--- commit-msg.sh
+++ commit-msg.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 #
-# An example hook script to check the commit log message.
 # Called by "git commit" with one argument, the name of the file
 # that has the commit message.  The hook should exit with non-zero
 # status after issuing an appropriate message if it wants to stop the
 # commit.  The hook is allowed to edit the commit message file.
 #
 # To enable this hook, rename this file to "commit-msg".
-grep -E '^L(\d+): [ A-Za-z0-9-]+ \(\d+ms\)$' $1 || {
-	echo >&2 "Incorrect commit message. Sample:\n\n    L123: LeetCode Problem Title (456ms)\n"
+grep -E '^((Hook: .*)|(Docs: .*)|(L(\d+): [ A-Za-z0-9-]+ \(\d+ms\)))$' $1 || {
+	echo >&2 "Incorrect commit message. Sample:\n\n    L123: LeetCode Problem Title (456ms)\n    Docs: Update section Foo    \n    Hook: Change something"
 	exit 1
 }
